f28557127d70446cbed31df4a68d27f6765014af
mbiscan <mbiscan@siue.edu>
2018-12-03 16:25:02 -0600

    Added to master

---
+++ Battleship/src/battleship/Ship.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author matt
+ */
+public class Ship {
+    int length;
+    Coordinate headCoordinate;
+    Coordinate tailCoordinate;
+    String color;
+    int hitCount = 0;
+    
+    
+    public boolean isSunk(Ship ship){
+        return ship.hitCount == ship.length;
+    }
+    
+    public void setLength(int length){
+        this.length = length;
+    }
+    
+//    public void setOrientation(String orientation){
+//        this.orientation = orientation;
+//    }
+    
+    public void setHeadCoordinateCol(Coordinate coordinate){
+        headCoordinate.xAxis = coordinate.xAxis;
+    }
+    
+    public void setHeadCoordinateRow(Coordinate coordinate){
+        headCoordinate.xAxis = coordinate.xAxis;
+    }
+    
+    public void setTailCoordinateCol(Coordinate coordinate){
+        headCoordinate.xAxis = coordinate.xAxis;
+    }
+    
+    public void setTailCoordinateRow(Coordinate coordinate){
+        headCoordinate.xAxis = coordinate.xAxis;
+    }
+    
+    public void setColor(String color){
+        
+    }
+}
